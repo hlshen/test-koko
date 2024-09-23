be36ef3f83e8cb4565eeeaf033a68441e2cf8c88
Harold Shen <hlshen@google.com>
2024-09-23 18:19:24 -0400

    ps1

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ setlocal
 
 ECHO Starting vsix code signing...
 
-PowerShell -File %KOKORO_BUILD_CONFIG_DIR%\sign_vsix.ps1
+PowerShell -File %KOKORO_ARTIFACTS_DIR%\github\test-koko\sign_vsix.ps1
 
 EXIT /b %ERRORLEVEL%
