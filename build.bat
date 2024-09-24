5324990a31fee521825be20c7c962338fbc2f54c
Harold Shen <hlshen@google.com>
2024-09-23 22:47:07 -0400

    signing

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ setlocal
 
 ECHO Starting vsix code signing...
 
-PowerShell -File %KOKORO_ARTIFACTS_DIR%\github\test-koko\sign_vsix.ps1
+ksigntool sign GOOGLE_EXTERNAL /v /debug /t http://timestamp.digicert.com %KOKORO_ARTIFACTS_DIR%\github\test-koko\fdc-latest.vsix
+
+signtool verify /pa /all %KOKORO_ARTIFACTS_DIR%\github\test-koko\fdc-latest.vsix
 
 EXIT /b %ERRORLEVEL%
